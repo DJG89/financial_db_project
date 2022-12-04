9e56f6924d359251dc95328d69d7ae2b1ddafa69
DJ <dgallowjr@hotmail.com>
2022-12-04 11:02:46 +0800

    made some changes to balance_sheet_func

--- balance_sheet_func.py
+++ balance_sheet_func.py
@@ -25,8 +25,33 @@ def get_balance_sheet():
 	new_data= json.dumps(data)
 	df=pd.read_json(new_data)
 
-	print("displaying data momentarily..")
-	time.sleep(3)
-	
 	print(tabulate(df, headers='keys', tablefmt = 'pretty'))
 	print("done. data collected for you to view\n\n")
+
+	message = '''
+	do you want to commit this info
+	to the database? y/n \n
+	'''
+
+	answer = input(message)
+
+	if answer == 'y':
+		# have to manipulate data
+		# to fit into sqlite db
+		
+		desired_columns = ['calendarYear',
+			'symbol',
+			'totalAssets',
+			'totalLiabilities',
+			'netReceivables',
+			'retainedEarnings',
+			'totalStockholdersEquity',
+			'cashAndCashEquivalents']
+
+		new_df = df[desired_columns]
+		new_df_2 = new_df.copy()
+
+		# commit data
+		conn = sqlite3.connect('fake_data.db')
+		c = conn.cursor()
+		new_df_2.to_sql('Balance_Sheet', conn, if_exists='append', index = False)
